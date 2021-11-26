36d218d5386dc409d01a534f4e868124ee2116d2
asitaslan <asitaslan95@gmail.com>
2021-11-26 17:54:08 +0300

    change urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api.views import PracticeListCreateAPIView, PracticeDetailAPIView
 
 urlpatterns = [
-    path('practices/', PracticeListCreateAPIView.as_view(), name='practices_list'),
-    path('practices/detail/<pk>', PracticeDetailAPIView.as_view(), name='practices_detail')
+    path('practices/list/create', PracticeListCreateAPIView.as_view(), name='practices_list'),
+    path('practices/detail/update/delete/<pk>', PracticeDetailAPIView.as_view(), name='practices_detail')
 ]
